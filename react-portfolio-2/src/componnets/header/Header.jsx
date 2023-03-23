116257aefc705b41b5174e9f9b8e6361070d8425
sathira97 <sathiranimhana@gmail.com>
2023-03-24 00:06:14 +0530

    added-header

--- react-portfolio-2/src/componnets/header/Header.jsx
+++ react-portfolio-2/src/componnets/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Sathira Nimhana</h1>
+        <h5 className="text-light">Game Developer</h5>
+      </div>
+    </header>
   )
 }
 
