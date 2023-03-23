e8b49e2138673e96739b1a2e26a3ac6cf15a3ffc
sathira97 <sathiranimhana@gmail.com>
2023-03-23 22:08:59 +0530

    adding-componnets-in-app

--- react-portfolio-2/src/App.jsx
+++ react-portfolio-2/src/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import Header from './componnets/header/Header'
+import Nav from './componnets/nav/Nav'
+import About from './componnets/about/About'
+import Experience from './componnets/experience/Experience'
+import Services from './componnets/services/Services'
+import Portfolio from './componnets/portfolio/Portfolio'
+import Testi from './componnets/testimoniols/Testi'
+import Contact from './componnets/contact/Contact'
+import Footer from './componnets/footer/Footer'
 
-export const App = () => {
+const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testi />
+        <Contact />
+        <Footer />
+    </>
   )
 }
+
+export default App
