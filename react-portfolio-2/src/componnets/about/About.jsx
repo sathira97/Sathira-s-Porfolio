8f2eafe60d2400a302d635979771ffee7281d491
sathira97 <sathiranimhana@gmail.com>
2023-03-25 19:45:59 +0530

    added-and-corect-css-missing

--- react-portfolio-2/src/componnets/about/About.jsx
+++ react-portfolio-2/src/componnets/about/About.jsx
@@ -20,24 +20,33 @@ const About = () => {
 
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__cards'>
+            <article className='about__card'>
               <FaAward className='about__icons' />
               <h5>Experience</h5>
               <small>3+ Years Working</small>
             </article>
 
-            <article className='about__cards'>
+            <article className='about__card'>
               <FiUsers className='about__icons' />
               <h5>Clients</h5>
               <small>80+ Worldwide</small>
             </article>
 
-            <article className='about__cards'>
+            <article className='about__card'>
               <VscFolderLibrary className='about__icons' />
               <h5>Projects</h5>
               <small>100+ Completed</small>
             </article>
           </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla voluptatibus, 
+            labore cum maiores quis molestias sapiente accusantium quia architecto quae consequuntur 
+            consectetur assumenda ea placeat aperiam at voluptas. Nihil, autem.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+          
         </div>
       </div>
     </section>
