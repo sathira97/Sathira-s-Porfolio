f724d118d8ffb607f7063b2d18e0e323c507f945
sathira97 <sathiranimhana@gmail.com>
2023-03-31 03:34:19 +0530

    added-contact

--- react-portfolio-2/src/componnets/contact/Contact.jsx
+++ react-portfolio-2/src/componnets/contact/Contact.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>sathiranimhana@gmail.com</h5>
+            <a href='mailto:sathiranimhana@gmail.com' target="_blank">Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Sathira Nimhana</h5>
+            <a href='https://www.facebook.com/sathira.nimhana/' target="_blank">Send a message</a>
+          </article>
+
+        </div>
+
+        <form action=''>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name='message' rows="7" placeholder='Your Message' required />
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
