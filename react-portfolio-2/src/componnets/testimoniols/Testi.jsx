b26068816f31c253d301aa883c5fa6c706e3b52a
sathira97 <sathiranimhana@gmail.com>
2023-03-30 23:27:46 +0530

    added-extra-pakages

--- react-portfolio-2/src/componnets/testimoniols/Testi.jsx
+++ react-portfolio-2/src/componnets/testimoniols/Testi.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
 import './testi.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const data = [
+
+  {
+    avatar: AVTR1,
+    name: 'Client 1',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Quasi veniam nesciunt culpa omnis itaque, deleniti tempora dolorum, ut deserunt labore incidunt modi voluptate architecto minima. Sint earum id odio obcaecati'
+  },
+
+  {
+    avatar: AVTR2,
+    name: 'Client 1',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Quasi veniam nesciunt culpa omnis itaque, deleniti tempora dolorum, ut deserunt labore incidunt modi voluptate architecto minima. Sint earum id odio obcaecati'
+  },
+
+  {
+    avatar: AVTR3,
+    name: 'Client 1',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit Quasi veniam nesciunt culpa omnis itaque, deleniti tempora dolorum, ut deserunt labore incidunt modi voluptate architecto minima. Sint earum id odio obcaecati'
+  },
+
+]
 
 const Testi = () => {
   return (
-    <section id='testi'>Testi</section>
+    <section id='testi'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <article key={index} className='testimonial'>
+
+                <div className="client__avatar">
+                  <img src={avatar}  />
+                </div>
+
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
