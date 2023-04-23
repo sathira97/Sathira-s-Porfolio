fd69baadabfdc2083f96bd2b2daf352962b5f93f
sathira97 <sathiranimhana@gmail.com>
2023-04-23 10:13:39 +0530

    last-edits

--- react-portfolio-2/src/componnets/header/HeaderSocials.jsx
+++ react-portfolio-2/src/componnets/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {FaArtstation} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://linkedin.com' target=''><BsLinkedin /></a>
-        <a href='https://github.com' target=''><ImGithub /></a>
-        <a href='https://artstation.com/' target=''><FaArtstation /></a>
+        <a href='https://www.linkedin.com/in/sathira-nimhana/' target='_blank'><BsLinkedin /></a>
+        <a href='https://github.com/sathira97' target='_blank'><ImGithub /></a>
+        <a href='https://www.artstation.com/sathiranimahana' target='_blank'><FaArtstation /></a>
     </div>
   )
 }
